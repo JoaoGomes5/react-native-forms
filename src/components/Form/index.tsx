b7dafe28a5e89d7121df05a1a2b6b462bd19ac6f
Joao Gomes <joaopfg.2002@gmail.com>
2022-03-23 21:07:30 +0000

    Yup and React Hook Forms emplementation :tada:

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,43 +1,72 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import { Button } from '../Button';
-import { Input } from '../Input';
+import { ControlledInput } from '../ControlledInput';
 import { Container } from './styles';
+type FormData = {
+  name: string
+  email: string
+  password: string
+  passwordConfirm: string
+}
 
-const VALID_EMAIL_EXPRESSION = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+const schema = yup.object({
+  name: yup.string().required('Informa o email'),
+  email: yup.string().email('E-mail invalido').required('Informa o email'),
+  password: yup.string().min(6, 'A password deve ter no minimo 6 digitos'),
+  passwordConfirm: yup.string().oneOf([yup.ref('password'), null], 'As passwords não coecidem')
+})
+
 
 export function Form() {
+  const { handleSubmit, control, formState } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
 
-  function handleUserRegister() {
+  const { errors } = formState
 
+  function handleUserRegister(data: FormData) {
+    
   }
 
   return (
     <Container>
-      <Input
+      <ControlledInput
         icon="user"
         placeholder="Nome"
+        name='name'
+        control={control}
+        error={errors.name}
       />
-      <Input
+      <ControlledInput
         icon="mail"
         placeholder="E-mail"
-        keyboardType="email-address"
-        autoCapitalize='none'
-      />
-      <Input
+        name='email'
+        control={control}
+        error={errors.email}
+        />
+      <ControlledInput
+        secureTextEntry
         icon="lock"
         placeholder="Senha"
+        name='password'
+        control={control}
+        error={errors.password}
+        />
+      <ControlledInput
         secureTextEntry
-      />
-      <Input
         icon="lock"
         placeholder="Confirme a senha"
-        secureTextEntry
-      />
+        name='passwordConfirm'
+        control={control}
+        error={errors.passwordConfirm}
+        />
 
       <Button
         title="Cadastrar"
-        onPress={handleUserRegister}
+        onPress={handleSubmit(handleUserRegister)}
       />
     </Container>
   )
